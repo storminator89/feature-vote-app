27ab14390b5dc7f24083301b218069fa64ffdcb8
Patrick Meyhöfer <p.meyhoefer@gmail.com>
2024-07-05 15:47:09 +0200

    small changes

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../feature-vote-app/build')));
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../feature-vote-app/build/index.html'));
+});
 
 const app = express();
 app.use(cors());
@@ -368,5 +377,5 @@ app.post('/api/ideas/:ideaId/comments/:commentId/replies', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/config.js
+++ src/config.js
@@ -1,10 +1,2 @@
 // config.js
-// Funktion zur Ermittlung der Base URL
-function getBaseUrl() {
-    const { protocol, hostname, port } = window.location;
-    const basePort = port ? `:${port}` : '';
-    return `${protocol}//${hostname}${basePort}/api`;
-  }
-  
-  // Exportieren Sie die BASE_URL als Konstante
-  export const BASE_URL = getBaseUrl();
+export const BASE_URL = 'http://localhost:5000/api';
